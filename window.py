cf76dc50b163c6b69e7e63c3d3665de3cb0a22be
jiffies <lcqtdwj@gmail.com>
2014-08-31 17:24:33 +0800

    popup window placement algorithm

--- window.py
+++ window.py
@@ -8,6 +8,7 @@ import os.path
 WIDTH = 800
 HEIGHT = 280
 MOUSE_DETECT_INTERVAL = 100
+LEAVE_BORDER_WIDTH = 50
 
 class Popup(object):
     def __init__(self):
@@ -17,7 +18,7 @@ class Popup(object):
         self.scroll = Gtk.ScrolledWindow()
         self.scroll.add(self.web)
         self.popup.add(self.scroll)
-        #event
+        self.gravity=None
     
     def load_uri(self,url):
         print url
@@ -26,15 +27,14 @@ class Popup(object):
     def reload(self):
         self.web.reload()
     
-    #def start(self):
-        #self.show_all()
-        #Gtk.main()
 
 class MainWindow(Gtk.Window):
     def __init__(self):
         super(MainWindow,self).__init__()
         self.display = self.get_display()
         self.screen = self.get_screen()
+        self.screen_width = self.screen.width()
+        self.screen_height = self.screen.height()
         self.connect("delete-event", self._on_delete_event)
         self.iconify()
         self.show_all()
@@ -56,16 +56,30 @@ class Clip(GObject.GObject):
         self.main_win = main_win
         self.popup = popup
         self.primary=Gtk.Clipboard.get(Gdk.SELECTION_PRIMARY)
-        #self.pretext = None
-        #self.pre_timeout = False
         #self.primary.connect('owner-change',self._on_owner_change)
         
 
     def _is_out(self,x,y,center_pointer,width,height):
-        if x>center_pointer['x']-50 and x<(center_pointer['x']+width+50) and y>center_pointer['y']-50 and y<(center_pointer['y']+height+50):
-            return False
-        elif (x-center_pointer['x'])**2+(y-center_pointer['y'])**2>50*50:
+        gravity = self.popup.gravity
+        lbw=LEAVE_BORDER_WIDTH
+        if gravity == Gdk.Gravity.NORTH_WEST:
+            if center_pointer['x']-lbw <x<(center_pointer['x']+width+lbw) and center_pointer['y']-lbw <y<(center_pointer['y']+height+lbw):
+                return False
+        elif gravity==Gdk.Gravity.SOUTH_WEST:
+            if center_pointer['x']-lbw <x<(center_pointer['x']+width+lbw) and (center_pointer['y']-height-lbw) <y<(center_pointer['y']+lbw):
+                return False
+
+        elif gravity==Gdk.Gravity.NORTH_EAST:
+            if (center_pointer['x']-width-lbw) <x<(center_pointer['x']+lbw) and center_pointer['y']-lbw <y<(center_pointer['y']+height+lbw):
+                return False
+
+        elif gravity==Gdk.Gravity.SOUTH_EAST:
+            if (center_pointer['x']-width-lbw) <x<(center_pointer['x']+lbw) and (center_pointer['y']-height-lbw) <y<(center_pointer['y']+lbw):
+                return False
+        if (x-center_pointer['x'])**2+(y-center_pointer['y'])**2>lbw*lbw:
             return True 
+        else:
+            return False
 
     def _check_mouse(self,center):
         s,x,y,m=self.main_win.display.get_pointer()
@@ -76,6 +90,33 @@ class Clip(GObject.GObject):
         else:
             return True
 
+    def _placement(self,click_x,click_y):
+        place_x=None
+        place_y=None
+        gravity=None
+        w,h=self.popup.popup.get_size()
+        if self.main_win.screen_width-click_x > w:
+            if self.main_win.screen_height-click_y > h:
+                gravity=Gdk.Gravity.NORTH_WEST
+                place_x=click_x
+                place_y=click_y
+            else:
+                gravity=Gdk.Gravity.SOUTH_WEST
+                place_x=click_x
+                place_y=click_y-h
+        else:
+            if self.main_win.screen_height-click_y > h:
+                gravity=Gdk.Gravity.NORTH_EAST
+                place_x=click_x-w
+                place_y=click_y
+            else:
+                gravity=Gdk.Gravity.SOUTH_EAST
+                place_x=click_x-w
+                place_y=click_y-h
+        self.popup.popup.set_gravity(gravity)
+        self.popup.gravity = gravity
+        self.popup.popup.move(place_x,place_y)
+
     def _on_owner_change(self):
         text = self.primary.wait_for_text()
         if text:
@@ -84,27 +125,25 @@ class Clip(GObject.GObject):
             print("No text on the clipboard.")
             return False
         self.primary.set_text('',0)
-        #if text == self.pretext and not self.pre_timeout:
-            #return
-        #elif self.pre_timeout:
-            #self.primary.clear()
-        #self.pretext = text
         s,x,y,m=self.main_win.display.get_pointer()
         print "x= %f,y=%f" % (x,y)
         results=youdaoQuery.gettext(text)
         fileName=youdaoQuery.creat_file(text,results)
         uri = 'file://'+os.path.join(self.main_win.dir,fileName)
         self.popup.load_uri(uri)
-        self.popup.popup.move(x,y)
+        self._placement(x,y)
         self.popup.popup.show_all()
         center={'x':x,'y':y}
         Gdk.threads_add_timeout(GLib.PRIORITY_DEFAULT_IDLE,MOUSE_DETECT_INTERVAL,self._check_mouse,center)
-        
+
+
+
 
 def main():
     pop=Popup()
     win=MainWindow()
     clip=Clip(win,pop)
+    #record client thread start
     rc=record.RecordClient(clip)
     win.rc = rc
     rc.start()
