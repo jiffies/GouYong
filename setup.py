186fc92ba05df6a136752ce743df2c29e0ff616b
jiffies <lcqtdwj@gmail.com>
2014-09-06 17:27:55 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+from setuptools import setup,find_packages
+
+setup(
+    name = "GouYong",
+    version = '0.1',
+
+    install_requires = ['PyGObject','python-xlib'],
+    #package_dir = {
+        #'dict':'dict',
+        #'cache':'cache',
+        #},
+        ##'':'src',
+        ##'':'lib',},
+    packages = find_packages(),
+    #include_package_data=True,
+    entry_points = {
+        'gui_scripts':[
+                'GouYong = GouYong.src.window:main'
+            ]
+        },
+
+    #data_files = [
+            #('dict',['dict/*']),
+            #('cache',['cache/*']),
+        #],
+    package_data = {
+        '':['*.png','*.temp','*.desktop'],
+        'GouYong':['cache/*','dict/*/*'],
+        },
+    author = "Jiffies",
+    author_email = "lcqtdwj@gmail.com",
+    description = "A dict application run in Linux",
+    license = "GPL",
+    url = "https://github.com/jiffies/GouYong",
+        )
