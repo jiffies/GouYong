52090f80dffe8f39bed240d47e7383f1f95f7565
jiffies <lcqtdwj@gmail.com>
2014-09-12 23:24:50 +0800

    v0.2

--- setup.py
+++ setup.py
@@ -13,31 +13,34 @@ def get_data_files():
             data_files.append((os.path.join(sys.prefix,path[path.find('/')+1:]),[os.path.join(path,file) for file in files]))
     return data_files
 
+if __name__=="__main__":
+    setup(
+        #setup_requires = [ "setuptools_git >= 0.3", ],
+        name = "GouYong",
+        version = '0.2',
+        packages = find_packages(),
+        #include_package_data=True,
+        entry_points = {
+            'gui_scripts':[
+                    'GouYong = GouYong.src.window:main'
+                ]
+            },
 
-setup(
-    name = "GouYong",
-    version = '0.1',
-    packages = find_packages(),
-    entry_points = {
-        'gui_scripts':[
-                'GouYong = GouYong.src.window:main'
-            ]
-        },
-
-    package_data = {
-        '':['*.temp'],
-        #'GouYong':['cache/*','dict/*/*'],
-        },
-    data_files = get_data_files(),
-    author = "Jiffies",
-    author_email = "lcqtdwj@gmail.com",
-    description = "A translation software on  linux",
-    license = "GPL",
-    url = "https://github.com/jiffies/GouYong",
-    classifiers=[
-        'Natural Language :: Chinese (Simplified)',
-        'Environment :: X11 Applications :: GTK',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 2.7',
-    ]
-)
+        package_data = {
+            '':['*.temp'],
+            #'GouYong':['cache/*','dict/*/*'],
+            },
+        data_files = get_data_files(),
+        #exclude_package_data = {'': ['.gitignore']},
+        author = "Jiffies",
+        author_email = "lcqtdwj@gmail.com",
+        description = "A translation software on  linux",
+        license = "GPL",
+        url = "https://github.com/jiffies/GouYong",
+        classifiers=[
+            'Natural Language :: Chinese (Simplified)',
+            'Environment :: X11 Applications :: GTK',
+            'Operating System :: POSIX :: Linux',
+            'Programming Language :: Python :: 2.7',
+        ],
+    )
