6d18b9ab3130aef4a25b496910a9f836b13b07da
jiffies <lcqtdwj@gmail.com>
2015-01-02 11:58:15 +0800

    escape url args

--- GouYong/src/youdaoQuery.py
+++ GouYong/src/youdaoQuery.py
@@ -1,15 +1,17 @@
 import urllib2
+import urllib
 import re
 from string import Template
 import os.path
 import os
 import log
 logger = log.get_logger(__name__)
+TIMEOUT = 5
 EXEPATH=os.path.split(os.path.realpath(__file__))[0]
 os.chdir(EXEPATH)
 CWD=os.getcwd()
 TEMPLATEFILE=os.path.join(CWD,"youdao.temp")
-QUERY = "http://dict.youdao.com/search?le=eng&q="
+QUERY = "http://dict.youdao.com/search?le=eng&" #q="
 PATTERN=r'(?s)(<div id="results">.*)<div id="ads" class="ads">'
 CACHEDIR=os.path.join(os.path.expanduser('~'),".cache","GouYong")
 try:
@@ -18,10 +20,16 @@ except OSError:
     logger.info("cache folder exists.")
 RESULTFILE="result.html"
 def gettext(word):
+    word = urllib.urlencode({'q':word})
     query = QUERY+word
-    response = urllib2.urlopen(query).read()
+    logger.info(query)
+    response = urllib2.urlopen(query,timeout=TIMEOUT).read()
     results=re.findall(PATTERN,response)
-    return results[0]
+    try:
+        return results[0]
+    except IndexError:
+        logger.info("query fail")
+        
     #return response
 def creat_file(word,results):
     template=Template(file(TEMPLATEFILE).read())
